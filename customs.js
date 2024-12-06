f4e5dd4e637cd4a19156af3795f1e2c48f4c3d7d
pritika335 <pritikaraut70@gmail.com>
2024-12-06 19:00:06 +0545

    first commit

---
+++ customs.js
@@ -0,0 +1,5 @@
+var name="pritika"
+    var age=20
+ // cnsole.log("my name  is "+ name+" age "+age)
+
+   console.log(`my name is ${name} age ${age}`)
---
+++ demo.js
@@ -0,0 +1,12 @@
+// username="pritika"
+// console. log(username)
+let name='pritika'
+let rol_num=22
+let email='pritikaraut1@gmail.com'
+let adress='hattiban'
+let phonenumber=9861669876
+document.getElementById('name').innerHTML =name
+document.getElementById('rol_num').innerHTML =rol_num
+document.getElementById('email').innerHTML =email
+document.getElementById('adress').innerHTML =adress
+document.getElementById('phone').innerHTML =phonenumber
